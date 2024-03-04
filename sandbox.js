50dc5d4010e06b210d14c48d9dd6fffa022e0937
Wenying Chang <changwenyingnl@gmail.com>
2024-03-04 20:05:38 +0100

    tutorial 2

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var FirstName = 'Wenying';
+console.log(FirstName);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,9 @@
+const FirstName = 'Wenying';
+console.log(FirstName);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach((input)=>{
+    console.log(input);
+});
