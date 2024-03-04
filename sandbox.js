be24fe85695fc14748f13c491c16c4f5d3357ec5
Wenying Chang <changwenyingnl@gmail.com>
2024-03-04 20:36:50 +0100

    tutorial 3

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,21 @@
-var string = 'wenying';
-var number = 30;
-var isLoading = false;
-string = 'string';
-isLoading = true;
-number = 29;
-var circ = function (diameter) { return diameter * Math.PI; };
-console.log(circ(10));
+//arrays
+var names = ['wenying', 'ingrid'];
+names.push('Bing');
+var numbers = [1, 2, 3];
+numbers.push(4);
+var mixed = ['wen', 20, 'bing', 30];
+mixed.push(30);
+mixed[0] = 30;
+//objects
+var objects = {
+    name: 'wenying',
+    age: 30,
+};
+objects.name = 'ying';
+//objects.name = 30;
+//objects.id=30;
+objects = {
+    name: 'bing',
+    age: 29,
+    //skills:"phd",
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,10 +1,28 @@
-let string = 'wenying';
-let number = 30;
-let isLoading = false;
+//arrays
 
-string = 'string';
-isLoading = true;
-number = 29;
+let names = ['wenying', 'ingrid'];
+names.push('Bing');
 
-const circ = (diameter: number) => { return diameter * Math.PI};
-console.log(circ(10));
+let numbers =[1, 2, 3];
+numbers.push(4);
+
+let mixed = ['wen', 20, 'bing', 30];
+mixed.push(30);
+mixed[0]=30;
+
+//objects
+
+let objects = {
+    name: 'wenying',
+    age: 30,
+}
+
+objects.name = 'ying';
+//objects.name = 30;
+//objects.id=30;
+
+objects = {
+    name: 'bing',
+    age: 29,
+    //skills:"phd",
+}
