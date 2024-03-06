83ec93fd794e39bdc0394626af0badbd3e483da2
Wenying Chang <changwenyingnl@gmail.com>
2024-03-06 09:38:40 +0100

    Tutorial #10 - Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,14 +1,35 @@
-type StringOrNum = string | number;
-type ObjWithName = {name: string, uuid: StringOrNum};
+//Function Signatures//
 
-const logDetails = (uuid: StringOrNum, item: string) => {
-        console.log(`${item} has a uuid of ${uuid}`);
+//example 1
+let greet: (a:string, b:string) => void;
+greet = (name: string, greetings: string) => {
+    console.log(`${name} says ${greetings}`);
 };
 
-logDetails(30, "Box");
+greet("wen", "hello");
 
-const greet = (user: ObjWithName) => {
-    console.log(`${user.name} says hello`);
+//example
+
+let calc: (a:number, b:number, c:string) => number;
+
+calc = (numOne: number, numTwo: number, action:string) => {
+    if (action === 'add'){
+        return numOne + numTwo
+    } else {
+        return numOne - numTwo
+    }
+
 };
+console.log(calc(3, 5, "add"));
 
-greet({name:"wenying", uuid: 30});
+//example3
+
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number};
+
+logDetails = (dutch: person) => {
+    console.log(`${dutch.name} is ${dutch.age} years old`);
+}
+
+logDetails({name: "Jorrit", age: 10});
