33cf9a2f09fef9e20aa42a7f79a0cbcfd3536d5f
Wenying Chang <changwenyingnl@gmail.com>
2024-03-06 09:09:35 +0100

    Tutorial #9 Type Aliases

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,28 +1,9 @@
 "use strict";
-//Define a type function
-let greet;
-greet = () => {
-    console.log('hello');
+const logDetails = (uuid, item) => {
+    console.log(`${item} has a uuid of ${uuid}`);
 };
-const add = (a, b) => {
-    console.log(a + b);
+logDetails(30, "Box");
+const greet = (user) => {
+    console.log(`${user.name} says hello`);
 };
-add(3, 5);
-const deduct = (a, b, c) => {
-    console.log(a, b, c);
-};
-deduct(3, 5, "hi");
-//make c optional by adding question mark
-const optional = (a, b, c) => {
-    console.log(a, b, c);
-};
-optional(1, 2); // c becomes undefined
-optional(3, 4, 5);
-const test = (a = 4, b = 10) => {
-    console.log(a * b);
-};
-const minus = (a, b) => {
-    return a - b;
-};
-let results = minus(20, 12);
-console.log(results);
+greet({ name: "wenying", uuid: 30 });
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,40 +1,14 @@
-//Define a type function
+type StringOrNum = string | number;
+type ObjWithName = {name: string, uuid: StringOrNum};
 
-let greet: Function;
-
-greet = () => {
-    console.log('hello');
+const logDetails = (uuid: StringOrNum, item: string) => {
+        console.log(`${item} has a uuid of ${uuid}`);
 };
 
+logDetails(30, "Box");
 
-const add = (a: number, b: number) => {
-    console.log(a+b)
-}
-
-add(3,5);
-
-
-const deduct = (a: number, b: number, c: number|string) => {
-    console.log(a, b, c )
+const greet = (user: ObjWithName) => {
+    console.log(`${user.name} says hello`);
 };
 
-deduct(3,5,"hi");
-
-
-//make c optional by adding question mark
-const optional = (a: number, b: number, c?: number|string) => {
-    console.log(a, b, c)
-};
-
-optional (1, 2); // c becomes undefined
-optional (3, 4, 5);
-
-const test = (a: number = 4, b: number = 10) => {
-    console.log(a*b);
-};
-
-const minus = (a: number, b: number):number => {
-    return a-b;
-}
-let results = minus (20,12);
-console.log(results)
+greet({name:"wenying", uuid: 30});
