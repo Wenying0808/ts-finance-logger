dc968b3c09e1222ef00387f5667c6605548aed5b
Wenying Chang <changwenyingnl@gmail.com>
2024-03-06 09:00:03 +0100

    tutorial 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,4 +1,28 @@
 "use strict";
-let age;
-age = 20;
-console.log(age);
+//Define a type function
+let greet;
+greet = () => {
+    console.log('hello');
+};
+const add = (a, b) => {
+    console.log(a + b);
+};
+add(3, 5);
+const deduct = (a, b, c) => {
+    console.log(a, b, c);
+};
+deduct(3, 5, "hi");
+//make c optional by adding question mark
+const optional = (a, b, c) => {
+    console.log(a, b, c);
+};
+optional(1, 2); // c becomes undefined
+optional(3, 4, 5);
+const test = (a = 4, b = 10) => {
+    console.log(a * b);
+};
+const minus = (a, b) => {
+    return a - b;
+};
+let results = minus(20, 12);
+console.log(results);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,4 +1,40 @@
-let age: number;
-age = 20;
+//Define a type function
 
-console.log(age);
+let greet: Function;
+
+greet = () => {
+    console.log('hello');
+};
+
+
+const add = (a: number, b: number) => {
+    console.log(a+b)
+}
+
+add(3,5);
+
+
+const deduct = (a: number, b: number, c: number|string) => {
+    console.log(a, b, c )
+};
+
+deduct(3,5,"hi");
+
+
+//make c optional by adding question mark
+const optional = (a: number, b: number, c?: number|string) => {
+    console.log(a, b, c)
+};
+
+optional (1, 2); // c becomes undefined
+optional (3, 4, 5);
+
+const test = (a: number = 4, b: number = 10) => {
+    console.log(a*b);
+};
+
+const minus = (a: number, b: number):number => {
+    return a-b;
+}
+let results = minus (20,12);
+console.log(results)
